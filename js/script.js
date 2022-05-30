a6f1c7fcc3194e4a4442c5af9e752b6eaccd547e
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-05-30 15:19:34 +0200

    add parallax carousel

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
+
+
 /**
- * 
+ * parallax carousel
  */
-// (() => {
-   
-// })();
-(function() {
+let imgArray = ['./img/carousel/1.jpg','./img/carousel/2.jpg','./img/carousel/3.jpg','./img/carousel/4.jpg'];
+const createCarousel = () => {
+    document.querySelector('.carouselContainer').style.backgroundImage =  `url(\'${imgArray[imgArray.length-1]}\')`;
+    let i = 0;
+    function slide() {
+        if(i == imgArray.length){
+            i = 0;
+        }
+        console.log("imgArray[i++%imgArray.length] = value =  "+imgArray[i]);
+        document.querySelector('.carouselContainer').style.backgroundImage = `url(\'${imgArray[i]}\')`;
+        i++;
+    }
+    setInterval(slide, 4000);
+}
+createCarousel();
+document.querySelector('.carouselContainer').setAttribute("style","background-attachment:fixed; background-position:center; background-repeat:no-repeat; background-size:cover;");
 
 /*
 *   Change mode of page on button click
 */
 
-document.getElementById("mode").setAttribute("style","width:50px; height:50px; border:1px solid black; border-radius:50%;bottom:30px;right:30px;");
-
-document.getElementById("mode").addEventListener("click",()=>{
+function changeMode(){
     var body = document.querySelector("body");
     if (body.className !== 'toggled') {
         body.style.backgroundColor="black";
@@ -24,14 +36,14 @@ document.getElementById("mode").addEventListener("click",()=>{
         body.style.color="black";
         body.className = '';
     }    
+}
+
+
+Array.from(document.querySelectorAll(".letter")).forEach(el => {
+   el.innerText = randomLetter();
 });
 
-
-
-
-
-
-})();
-// ul.nav > li {
-//     display: inline;
-// }
+function randomLetter(){
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+    return alphabet[Math.floor(Math.random() * alphabet.length)]
+}
